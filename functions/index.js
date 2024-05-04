4d06f6055e2b4d92810d4c6a42a2f53a3d525fcf
ntjson2 <ntjson@gmail.com>
2024-05-04 10:37:02 -0600

    init

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,6 @@ const {onRequest} = require("firebase-functions/v2/https");
 const pug = require('pug');
 
 
-
 exports.test = onRequest((request, response) => {
 
     let template = pug.compileFile('views/index.pug');
@@ -12,6 +11,34 @@ exports.test = onRequest((request, response) => {
         name: request.query.name, 
         major: request.query.major,
         quote: 'Howdy!'});
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end(markup);
+});
+
+exports.about = onRequest((request, response) => {
+    let template = pug.compileFile('views/about.pug');
+    let markup = template({});
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end(markup);
+});
+
+exports.home = onRequest((request, response) => {
+    let template = pug.compileFile('views/home.pug');
+    let markup = template({});
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end(markup);
+});
+
+exports.contact = onRequest((request, response) => {
+    let template = pug.compileFile('views/contact.pug');
+    let markup = template({});
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end(markup);
+});
+
+exports.side = onRequest((request, response) => {
+    let template = pug.compileFile('views/side.pug');
+    let markup = template({});
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.end(markup);
 });
